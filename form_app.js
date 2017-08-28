97576bb2a89e12caa696c5c14817d53a71db462c
David Johnson <davidjohnson@Lees-MacBook-Pro.local>
2017-08-28 16:08:02 -0700

    Commented out app.js for revisions

---
+++ form_app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var formInfo = function() {
+  var form = document.getElementsByTagName('form')[0];
+  // use event listener to remove form as follows
+  // document.getElementById('form').style.display = "none";
+  // var ques1 = document.createElement('label');ques1.innerHTML = 'What is your name?';
+  // var ques2 = document.createElement('label');ques2.innerHTML = 'What is your dog\'s name?';
+  // var ques3 = document.createElement('label');ques3.innerHTML = 'What type of a dog do you have?';
+  // var ques4 = document.createElement('label');ques4.innerHTML = 'Were do you live?';
+  // var ques5 = document.createElement('label');ques5.innerHTML = 'What is your dog\'s star sign?';
+  // var field = document.getElementById('field');
+  // console.log(ques5);
+  // field.appendChild(ques5);
+};
+
+formInfo();
