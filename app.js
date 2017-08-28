4522e6a9b265fd7cfe3c22074227aaa73b8bea4c
CHELSEA DOLE <chelseadole@gmail.com>
2017-08-28 09:46:30 -0700

    beginning Dog constructor, making new Dog objects

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 'use strict';
 
-var Dog = new Object(name, birthday, breed, loc){
+function Dog(name, starSign, breed, loc) {
   this.name = name;
   this.birthday = birthday;
   this.breed = breed;
   this.loc = loc;
-  
+  this.multipleChoice = [];
+};
+
+var Evi = new Dog('Evi', 'Leo', 'German Shepherd', 'Lake City', [c, d, d]);
+var Daisy = new Dog('Daisy', 'Capricorn', 'Beagle', 'Ballard', [a, b, b]);
+var Sadie = new Dog('Sadie', 'Cancer', 'Mix (Unknown)', 'Capitol Hill', [c, c, b]);
+var Robb = new Dog('Robb', 'Aries', 'Australian Shepherd', 'Queen Anne', [d, d, c]);
+var 
