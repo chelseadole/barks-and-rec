8c070ef9656a3a781c6e3a602851bbfc4702bec2
David Johnson <davidjohnson@Lees-MacBook-Pro.local>
2017-08-28 16:43:07 -0700

    form section is complete

--- form_app.js
+++ form_app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function removeForm(event){
+  event.preventDefault();
   document.getElementById('form').style.display = 'none';
 }
 document.getElementById('form').addEventListener('submit',removeForm);
