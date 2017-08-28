764ee8ed2ee73c2515e1c2ba0a077bd818a566b8
David Johnson <davidjohnson@Lees-MacBook-Pro.local>
2017-08-28 16:57:49 -0700

    working on multi questions

--- form_app.js
+++ form_app.js
@@ -5,3 +5,6 @@ function removeForm(event){
   document.getElementById('form').style.display = 'none';
 }
 document.getElementById('form').addEventListener('submit',removeForm);
+
+var questions = ['ques1', 'ques2', 'ques3'];
+var answers = [[1,2,3], [1,2.3]]
