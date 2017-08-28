c7c625c215b12d1df9e132bc513ae15248fb13ec
CHELSEA DOLE <chelseadole@gmail.com>
2017-08-28 11:00:44 -0700

    finishing whichDog, compareDogs functions to find sorted array of best dog matches

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
 'use strict';
 
-function Dog(name, starSign, breed, loc) {
+function Dog(name, starSign, breed, loc, quizResults) {
   this.name = name;
-  this.birthday = birthday;
+  this.starSign = starSign;
   this.breed = breed;
   this.loc = loc;
-  this.multipleChoice = [];
+  this.quizResults = quizResults;
+  builtInDogs.push(this);
 };
 
-var Evi = new Dog('Evi', 'Leo', 'German Shepherd', 'Lake City', [c, d, d]);
-var Daisy = new Dog('Daisy', 'Capricorn', 'Beagle', 'Ballard', [a, b, b]);
-var Sadie = new Dog('Sadie', 'Cancer', 'Mix (Unknown)', 'Capitol Hill', [c, c, b]);
-var Robb = new Dog('Robb', 'Aries', 'Australian Shepherd', 'Queen Anne', [d, d, c]);
-var 
+var builtInDogs = [];
+var myDogScores = [];
+
+var myDog = new Dog('myDog', 'Aries', 'Shiba Inu', 'Code Fellows', [1, 2, 3]);
+
+var Evi = new Dog('Evi', 'Leo', 'German Shepherd', 'Lake City', [3, 4, 4]);
+var Daisy = new Dog('Daisy', 'Capricorn', 'Beagle', 'Ballard', [1, 2, 2]);
+var Sadie = new Dog('Sadie', 'Cancer', 'Mix (Unknown)', 'Capitol Hill', [3, 3, 2]);
+var Robb = new Dog('Robb', 'Aries', 'Australian Shepherd', 'Queen Anne', [4, 4, 3]);
+var Zack = new Dog('Zack', 'Scorpio', 'Black Lab', 'West Seattle', [1, 1, 4]);
+var Monchi = new Dog('Monchi', 'Taurus', 'Terrier Mix', 'Edmonds', [2, 3, 3]);
+
+var makeScores = function() {
+  for (var i = 0; i < builtInDogs.length; i++) {
+    myDogScores.push(compareDogs(myDog, builtInDogs[i]));
+  }
+};
+
+var compareDogs = function(dog1, dog2) {
+  var total = 0;
+  for (var i = 0; i < dog1.quizResults.length; i++) {
+    total += Math.abs(dog1.quizResults[i] - dog2.quizResults[i]);
+  }
+  return total;
+};
+
+var whichDog = function() {
+  myDogScores.sort(function(a, b) {
+    console.log('hello');
+    return (a - b);
+  });
+  console.log('second level');
+  return myDogScores;
+};
