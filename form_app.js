7d9c77684fb29dcb110b42fdd617c12aec1e1c62
David Johnson <davidjohnson@Lees-MacBook-Pro.local>
2017-08-28 17:13:07 -0700

    Added for loop for multiple choice

--- form_app.js
+++ form_app.js
@@ -7,4 +7,12 @@ function removeForm(event){
 document.getElementById('form').addEventListener('submit',removeForm);
 
 var questions = ['ques1', 'ques2', 'ques3'];
-var answers = [[1,2,3], [1,2.3]]
+var answers = [[1,2,3], [1,2.3]];
+var form = document.getElementById('formOne');
+for (var i = 0; i > questions.length; i++){
+  var lable = document.createElement('lable');
+  lable.innerHtml = questions[i];
+  form.appendChild(lable);
+  console.log(questions[i]);
+  var inputChoice = document.createElement('input');
+}
