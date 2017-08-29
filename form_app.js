68d3619d0892e9373688edcd20d34f6a081e66f0
David Johnson <davidjohnson@Lees-MacBook-Pro.local>
2017-08-29 11:20:04 -0700

    Finished multiple choice and appended to DOM

--- form_app.js
+++ form_app.js
@@ -2,30 +2,53 @@
 
 function removeForm(event){
   event.preventDefault();
+  debugger;
+  answer1 = event.target.getElementsByTagName('input').ques1.value;
   document.getElementById('form').style.display = 'none';
 }
 document.getElementById('form').addEventListener('submit',removeForm);
 
 var dogQuestions = [{
-  question: "Which of these does your dog likes most?",
-  choices: ["Just walk", "Playing fetch", " Swiming", "Playing with other dogs"]
+  question: 'Which of these does your dog likes most?',
+  choices: ['Just walk', 'Playing fetch', ' Swiming', 'Playing with other dogs']
 },
 {
-  question:"What is your dog's ideal walk time?",
-  choices: ["5-10 mins ", "10-20 mins", "20-30 mins", "30+ mins "]
-}
+  question:'What is your dog\'s ideal walk time?',
+  choices: ['5-10 mins ', '10-20 mins', '20-30 mins', '30+ mins ']
+},
+{
+  question:'How well does your dog get along with other dogs?',
+  choices: ['Not very well ', 'Good with one other dog', 'Great with a small group', 'My dog loves being part of a pack! The more the merrier. ']
+},
+{
+  question:'What sizes of dgos does your dog mesh with best?',
+  choices: ['Small dogs only ', 'Medium dogs only', 'Big dogs only', 'Any dog size is okay ']
+},
+{
+  question:'How energetic is your dog?',
+  choices: ['Not at all ', 'A little', 'Very energetic', 'Crazy ']
+},
+{
+  question:'What toy does your dog like most?',
+  choices: ['My dog prefers walking or playing without toys ', 'A ball is best, something to fetch', 'A stick or rope toy for tug-of-war', 'Multiple toys is best ']
+},
 ];
-for (var i = 0; i < questions.length; i ++){
+var form = document.getElementById('formOne');
+for (var i = 0; i < dogQuestions.length; i ++){
   var question = dogQuestions[i].question;
   console.log(question);
+  var label = document.createElement('label');
+  label.innerHTML = question + '<br>';
+  form.appendChild(label);
   var answers = dogQuestions[i].choices;
   console.log(answers);
-}
-var form = document.getElementById('formOne');
-for (var i = 0; i > questions.length; i++){
-  var lable = document.createElement('lable');
-  lable.innerHtml = questions[i];
-  form.appendChild(lable);
-  console.log(questions[i]);
-  var inputChoice = document.createElement('input');
-}
+
+  for ( var j = 0; j < 4; j ++){
+    var radio = document.createElement('input');
+    radio.type = 'radio';
+    radio.class = 'rad';
+    label.appendChild(radio);
+    label.innerHTML += answers[j] + '<br>';
+
+  }
+};
