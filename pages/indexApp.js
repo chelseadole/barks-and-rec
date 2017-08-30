b35d1900fcd00fbc8c1910360f0d17ff79200cdc
maxawolff <maxawolff@hotmail.com>
2017-08-30 13:30:19 -0700

    added funcitonality to the form and continued working on re-styling

---
+++ pages/indexApp.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var form = document.getElementsByTagName('form')[0];
+form.addEventListener('submit', storeInfo);
+
+function storeInfo(event){
+  event.preventDefault();
+  // console.log(form.elements['userName'].value);
+  // console.log(form.elements['dogName'].value);
+  localStorage.setItem('ownerName', form.elements['userName'].value);
+  localStorage.setItem('dogName', form.elements['dogName'].value);
+  location.assign('quiz.html');
+}
