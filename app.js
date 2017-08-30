6e316f465626f44c4074d6c45696764664d23d93
CHELSEA DOLE <chelseadole@gmail.com>
2017-08-30 15:43:15 -0700

    adding Asha dog object, adding concatenated names to h2 in resultsPage

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function Dog(name, starSign, breed, loc, picture, quizResults, bio) {
   this.quizResults = quizResults;
   this.bio = bio;
   this.dogScore = 0;
-  if (this.name != 'MyDog') {
+  if (this.name != localStorage.dogName) {
     builtInDogs.push(this);
   }
 };
@@ -22,7 +22,9 @@ var myDogScores = [];
 
 var radioAnswerArray = JSON.parse(localStorage.answersData);
 var formAnswerArray = JSON.parse(localStorage.formData);
-var myDog = new Dog('MyDog', formAnswerArray[2], formAnswerArray[0], formAnswerArray[1], 'My House', radioAnswerArray, 'bio-placeholder');
+var ownerName = localStorage.ownerName;
+var dogName = localStorage.dogName;
+var myDog = new Dog(dogName, formAnswerArray[2], formAnswerArray[0], formAnswerArray[1], 'My House', radioAnswerArray, 'bio-placeholder');
 
 var Evi = new Dog('Evi', 'Leo', 'German Shepherd', 'Lake City', '../dogImgs/germanshepherd-evi.jpg', [3, 4, 4, 4, 3, 3], 'Evi is a German Shepherd from Lake City. She likes swimming, 30+ minute walks, and her favorite activity is playing tug-of-war with a stick or rope. She is very energetic, likes to play with a large pack of dogs, and gets along best with big dogs.');
 
@@ -66,6 +68,8 @@ var Buddy = new Dog('Buddy', 'Leo', 'Pug', 'South Lake Union', '../dogImgs/pug.j
 
 var Molly = new Dog('Molly', 'Taurus', 'Australian Shepherd', 'Fremont', '../dogImgs/australian-2.jpg', [1, 2, 4, 3, 4, 1], 'Molly is an Australian Shepherd from Fremont. She prefers walking over swimming or fetch, 10-20 minute walks, and his favorite activity is playing with multiple toys. She is very energetic, likes to play with a large pack of dogs, and gets along best with small dogs.');
 
+var Asha = new Dog('Asha', 'Pisces', 'Husky', 'University District', '../dogImgs/asha.jpg', [3, 4, 3, 4, 4, 2], 'Asha is an Alaskan Husky and service dog from the University District. She likes swimming, 30+ minute walks, and her favorite activity is playing with a ball. She is crazy energetic, likes to play with a small group of dogs, and gets along well with dogs of any size.');
+
 //pushes match scores (from compareDogs) to myDogScores
 var makeScores = function() {
   for (var i = 0; i < builtInDogs.length; i++) {
@@ -99,7 +103,14 @@ var whichDog = function() {
   appendToResults();
 };
 
+function capitalize(string) {
+  return string[0].toUpperCase() + string.slice(1);
+};
+
 var appendToResults = function(){
+  var targetLabel = document.getElementById('topMatchLabel');
+  targetLabel.innerHTML = capitalize(ownerName) + ' and ' + capitalize(dogName) + '\'s Top Matches:';
+
   var firstH3 = document.getElementById('firstH3');
   firstH3.innerHTML = 'Top Match: ' + myDogScores[0].name;
   var secondH3 = document.getElementById('secondH3');
